b638331b8f6a7d08b513ca1b286bb09e9203e652
Fernando Mangold <fernandomangold21@gmail.com>
2022-03-12 18:17:50 -0300

    initial commit

---
+++ js/clase1.js
@@ -0,0 +1,26 @@
+// js uses camel case
+// var is not recomended
+
+var nombrePersona = "Andres";
+
+// homework: difference between let and var
+
+let nombre = "Juan";
+
+const PERSONA = "Roberto";
+
+// ; is used for good practice
+
+console.log(nombrePersona)
+
+// to run js files in console: node {path to file}
+
+// strings formateados
+// we use backticks
+
+console.log(`${nombrePersona} ${nombre}`)
+
+let nombreIngresado = prompt("Por favor ingrese su nombre")
+let apellidoIngresado = prompt("Por favor ingrese su apellido")
+
+console.log(`${nombreIngresado} ${apellidoIngresado}`)
